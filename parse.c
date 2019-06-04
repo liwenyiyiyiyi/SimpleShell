d454d9419a01b7ae1fb40a4eccaff24c81da187f
IssacZF <34516609+IssacZF@users.noreply.github.com>
2019-06-04 14:57:08 +0800

    Update parse.c

--- parse.c
+++ parse.c
@@ -1,3 +1,16 @@
 #include "parse.h"
-
-
+char* readCommandLine(char *cmdLine)
+{
+    char *cmdBuf;
+    cmdBuf = readline(" ");
+    if (strlen(cmdBuf) != 0)
+    {
+        strcpy(cmdLine,cmdBuf);
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+    
+}
