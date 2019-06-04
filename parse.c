d0c9cee374df17b96588988b08fc81c0ab1ddbfe
IssacZF <34516609+IssacZF@users.noreply.github.com>
2019-06-04 18:04:34 +0800

    Update parse.c

--- parse.c
+++ parse.c
@@ -40,3 +40,68 @@ bool isBuiltInCommand(char** cmd) {
     return 0;
   }
 }
+char **redirectionCommand(char **cmd)
+{
+    int flag_In = 0;
+    int flag_Out = 0;
+    int flag_Outplus = 0;
+    int counter = 0;
+    int length;
+    int file_In;
+    int file_Out;
+    int file_Outplus;
+    char In[MAX];
+    char Out[MAX];
+    char Outplus[MAX];
+    char *token = cmd[0];
+    while (token)
+    {
+        length = strlen(token);
+        if (strncmp(token, "<", length) == 0)
+        {
+            cmd[counter] = NULL;
+            flag_In = 1;
+            strcpy(In, cmd[counter + 1]);
+        }
+        if (strncmp(token, ">", length) == 0)
+        {
+            cmd[counter] = NULL;
+            flag_Out = 1;
+            strcpy(Out, cmd[counter + 1]);
+        }
+        if (strncmp(token, ">>", length) == 0)
+        {
+            cmd[counter] = NULL;
+            flag_Outplus = 1;
+            strcpy(Outplus, cmd[counter + 1]);
+        }
+        counter++;
+        token = cmd[counter];
+    }
+    if (!flag_In && !flag_Out && !flag_Outplus)
+    {
+        return cmd;
+    }
+    if (flag_In)
+    {
+
+        file_In = open(In, O_RDONLY);
+        dup(file_In);
+        close(file_In);
+    }
+    if (flag_Out)
+    {
+
+        file_Out = open(Out, O_WRONLY | O_CREAT);
+        dup(file_Out);
+        close(file_Out);
+    }
+    if (flag_Outplus)
+    {
+
+        file_Outplus = open(Outplus, O_WRONLY | O_CREAT | O_APPEND);
+        dup(file_Outplus);
+        close(file_Outplus);
+    }
+    return cmd;
+}
